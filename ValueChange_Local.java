f94c75fe6c47ed62079a8f1145cded6fb3975ec8
Knut B <knut.berling@gmx.de>
2024-08-26 18:55:14 +0200

    y

--- ValueChange_Local.java
+++ ValueChange_Local.java
@@ -1,10 +1,10 @@
 class ValueChange {
 
     void valueChange() {
-        int a = 1;
+        int a = 0;
         int b = 10 / a;
 	int normal = 0;
-	int c = 1;
+	int c = 0;
 	int d = 10 / c;
     }
 }
